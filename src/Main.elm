8935ab7a4890ed84c020181661861ab6c842f00e
Joshua Clayton <joshua.clayton@gmail.com>
2016-04-07 17:18:52 -0400

    Static question render

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,49 @@
 module Main (..) where
 
 import Html exposing (..)
+import Html.Attributes exposing (class, type', placeholder, value, id, for)
 
 
 main : Html
 main =
-  text "Hello world!"
+  view
+
+
+view : Html
+view =
+  div
+    [ class "container" ]
+    [ header [] [ h1 [] [ text "Questions" ] ]
+    , section
+        []
+        [ questionMetadata
+        , questionHeader
+        , questionSubmissionForm
+        ]
+    ]
+
+
+questionMetadata : Html
+questionMetadata =
+  dl
+    []
+    [ dt [] [ text "Category" ]
+    , dd [] [ text "Oscar songs in difficult words" ]
+    , dt [] [ text "Difficulty" ]
+    , dd [] [ text "600" ]
+    ]
+
+
+questionHeader : Html
+questionHeader =
+  h2 [] [ text "1997:A personal cardiac organ shall endure" ]
+
+
+questionSubmissionForm : Html
+questionSubmissionForm =
+  form
+    []
+    [ label [ for "answer" ] [ text "What do you think?" ]
+    , input [ type' "text", placeholder "Enter your answer", id "answer" ] []
+    , input [ type' "submit", value "Submit" ] []
+    ]
